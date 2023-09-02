70f735ebed7cc84a5048cd1524381f3fa03a700b
AnithaK94 <kannananitha.94@gmail.com>
2023-09-02 20:43:23 +0530

    ClassesMoreThan5Students
    Solution to find all the classes that have at least five students.
    Return the result table in any order.

---
+++ ClassesMoreThan5Students.sql
@@ -0,0 +1,22 @@
+use leet_code_easy;
+# Create a Table
+
+Create table If Not Exists Courses (student varchar(255), class varchar(255))
+Truncate table Courses
+insert into Courses (student, class) values ('A', 'Math')
+insert into Courses (student, class) values ('B', 'English')
+insert into Courses (student, class) values ('C', 'Math')
+insert into Courses (student, class) values ('D', 'Biology')
+insert into Courses (student, class) values ('E', 'Math')
+insert into Courses (student, class) values ('F', 'Computer')
+insert into Courses (student, class) values ('G', 'Math')
+insert into Courses (student, class) values ('H', 'Math')
+insert into Courses (student, class) values ('I', 'Math')
+
+/* Write a solution to find all the classes that have at least five students.
+Return the result table in any order.  */
+
+Select class
+from Courses
+group by class
+having count(class) >= 5
