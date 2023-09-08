5d1aa308c5f049646c7a245719c08cd5f44a1691
AnithaK94 <kannananitha.94@gmail.com>
2023-09-08 21:57:35 +0530

    PercentageOfUsersAttendedAContest
    Solution to find the percentage of the users registered in each contest rounded to two decimals.
    Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.

---
+++ BasicJoins/PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,30 @@
+use sqlinterview;
+#Create a Table
+Create table If Not Exists Users (user_id int, user_name varchar(20));
+Create table If Not Exists Register (contest_id int, user_id int);
+Truncate table Users;
+insert into Users (user_id, user_name) values ('6', 'Alice');
+insert into Users (user_id, user_name) values ('2', 'Bob');
+insert into Users (user_id, user_name) values ('7', 'Alex');
+Truncate table Register;
+insert into Register (contest_id, user_id) values ('215', '6');
+insert into Register (contest_id, user_id) values ('209', '2');
+insert into Register (contest_id, user_id) values ('208', '2');
+insert into Register (contest_id, user_id) values ('210', '6');
+insert into Register (contest_id, user_id) values ('208', '6');
+insert into Register (contest_id, user_id) values ('209', '7');
+insert into Register (contest_id, user_id) values ('209', '6');
+insert into Register (contest_id, user_id) values ('215', '7');
+insert into Register (contest_id, user_id) values ('208', '7');
+insert into Register (contest_id, user_id) values ('210', '2');
+insert into Register (contest_id, user_id) values ('207', '2');
+insert into Register (contest_id, user_id) values ('210', '7');
+/* Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order. */
+Select reg.contest_id, round(
+  count(distinct user_id) * 100/(Select 
+  count(user_id)from Users),2) as percentage
+from Register as reg
+group by reg.contest_id
+order by percentage desc,
+         reg.contest_id ;
