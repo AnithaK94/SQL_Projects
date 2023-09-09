eeab10208717658ab41675e66ae19583ed4e6c69
AnithaK94 <kannananitha.94@gmail.com>
2023-09-09 14:01:55 +0530

    ImmediateFoodDeliveryII
    Solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.

---
+++ BasicJoins/ImmediateFoodDeliveryII.sql
@@ -0,0 +1,24 @@
+use sqlinterview;
+#Create a Table
+Create table If Not Exists Delivery (delivery_id int, customer_id int, order_date date, customer_pref_delivery_date date);
+Truncate table Delivery;
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('1', '1', '2019-08-01', '2019-08-02');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('2', '2', '2019-08-02', '2019-08-02');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('3', '1', '2019-08-11', '2019-08-12');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('4', '3', '2019-08-24', '2019-08-24');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('5', '3', '2019-08-21', '2019-08-22');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('6', '2', '2019-08-11', '2019-08-13');
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('7', '4', '2019-08-09', '2019-08-09');
+/*If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+The first order of a customer is the order with the earliest order date that the customer made. 
+It is guaranteed that a customer has precisely one first order.
+Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.*/
+
+select 
+   round(avg (order_date = customer_pref_delivery_date)* 100 ,2) as immediate_percentage
+From Delivery
+where (customer_id, order_date) in (
+Select customer_id,min(order_date)
+from Delivery
+group by customer_id
+);
